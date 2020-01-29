709c60179b8d8afb51946a024ae1e443a5389b8d
Kishan vekariya <kishan.vekariya@solutionanalysts.com>
2020-01-29 12:06:29 +0530

    first commit

---
+++ components/LoginComponent.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+
+export default class LoginComponent extends Component {
+    constructor() {
+        super()
+        this.state = { email: '', password: '' }
+    }
+    render() {
+        return <View style={styles.container}>
+            <View style={styles.firstContainer}><Text style={styles.loginText}>LOGIN</Text></View>
+            <View style={styles.secondContainer}>
+                <View style={styles.textFieldContainer}>
+                    <TextInput
+                        keyboardType='email-address'
+                        placeholder="Email"
+                        style={[styles.commoninput, styles.emailInput]}
+                        value={this.state.email}
+                        onChangeText={(email) => this.setState({ email })}>
+                    </TextInput>
+                    <TextInput
+                        secureTextEntry={true}
+                        placeholder="Password"
+                        style={[styles.commoninput, styles.passWordInput]}
+                        value={this.state.password}
+                        onChangeText={(password) => this.setState({ password })}>
+                    </TextInput>
+                    <TouchableOpacity style={styles.touchOpacity} onPress={this.onLogin}>
+                        <Text style={styles.buttonText}>login</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.thirdContainer}><Text>Forgot Password ?</Text></View>
+        </View>
+    }
+    onLogin = () => {
+        // Alert.alert("Login","Success")
+        fetch('http://35.160.197.175:3006/api/v1/user/login',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'email': this.state.email,
+                    'password': this.state.password
+                })
+            }).then((response) => {
+                if (response.status == 200) {
+                    return response.json()
+                } else {
+
+                }
+            }).then((responseJSON) => {
+                console.log(responseJSON);
+                Alert.alert('Success', 'Welcome! ' + responseJSON.firstName + ' ' + responseJSON.lastName, [
+                    {
+                        text: 'Okay',
+                        style: 'cancel'
+                    },
+                    {
+                        text: 'Cancel',
+                        style: 'destructive'
+                    },
+                ])
+            }
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'lightgray',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+    loginText: {
+        fontSize: 30
+    },
+    firstContainer: {
+        flex: 3,
+        top: 70,
+        // backgroundColor: 'cyan',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    secondContainer: {
+        flex: 3,
+        // backgroundColor: 'magenta',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    thirdContainer: {
+        flex: 4,
+        // backgroundColor: 'green',
+        alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    textFieldContainer: {
+        width: '100%',
+        height: 180,
+        // backgroundColor:'green',
+        alignItems: 'center'
+    },
+    commoninput: {
+        backgroundColor: 'white',
+        width: '80%',
+        paddingLeft: 10,
+        height: 50,
+        top: 10,
+        borderWidth: 1,
+        // borderBottomWidth:1,
+        borderColor: 'gray',
+        borderRadius: 10,
+    },
+    emailInput: {
+
+    },
+    passWordInput: {
+        top: 20
+    },
+    touchOpacity: {
+        borderRadius: 50,
+        top: 40,
+        height: 50,
+        width: '80%',
+        backgroundColor: 'rgb(255, 74, 0)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
